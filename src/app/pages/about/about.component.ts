d83d0d966997b9c464d5c1d225f22718bb760676
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2019-05-08 13:53:23 -0500

    Sección 5

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppDataService } from '../../services/app-data.service';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _service: AppDataService ) { }
 
   ngOnInit() {
   }
--- src/app/services/app-data.service.ts
+++ src/app/services/app-data.service.ts
@@ -11,16 +11,28 @@ export class AppDataService {
 
   info: AppData = {};
   load: boolean = false;
+  team: any[] = [];
 
   constructor( private http: HttpClient ) {
+    this.getAppData();
+    this.getTeamData();
+  }
 
+  private getAppData() {
     this.http
         .get( 'assets/data/app-data.json' )
         .subscribe( ( response: AppData ) => {
           this.info = response;
           this.load = true;
         });
+  }
 
+  private getTeamData() {
+    this.http
+        .get( 'https://angular-portafolio-ad054.firebaseio.com/equipo.json' )
+        .subscribe( ( response: any ) => {
+          this.team = response;
+        });
   }
 
 }
